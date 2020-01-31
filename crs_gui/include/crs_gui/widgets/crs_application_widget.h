df0e8a6f0d7bc56699ababbdec088a32558a30de
mpowelson <matthew.powelson@swri.org>
2020-01-31 16:56:32 -0600

    Cleanup and add selection widget

--- crs_gui/include/crs_gui/widgets/crs_application_widget.h
+++ crs_gui/include/crs_gui/widgets/crs_application_widget.h
@@ -21,17 +21,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
+#include <std_msgs/msg/string.hpp>
 #include <string>
-
-//#include <geometry_msgs/PoseStamped.h>
-//#include <ros/node_handle.h>
-//#include <ros/service_client.h>
-
-//#include <crs_msgs/AllowUserControlAction.h>
-//#include <actionlib/server/simple_action_server.h>
-
-//#include <crs_gui/widgets/manual_manipulation_widget.h>
-
 
 class QPushButton;
 class QStateMachine;
@@ -43,162 +34,35 @@ namespace Ui
 class CRSApplication;
 }
 
-//namespace crs_application
-//{
-//class ApplicationContextBase;
-//typedef typename std::shared_ptr<ApplicationContextBase> ApplicationContextBasePtr;
-//}
-
 namespace crs_gui
 {
 class PartSelectionWidget;
-//class JobSelectorWidget;
-//class TrajectoryApprovalWidget;
-//class DatabaseLogWidget;
- 
+class PolygonAreaSelectionWidget;
+
 class CRSApplicationWidget : public QWidget
 {
-Q_OBJECT
+  Q_OBJECT
 public:
-
   CRSApplicationWidget(rclcpp::Node::SharedPtr node,
                        QWidget* parent = nullptr,
-                       std::string database_directory = std::string(std::getenv("HOME")) + "/.local/share/offline_generated_paths");
-
-Q_SIGNALS:
-
-  void showWarningDialog(const QString& title,
-                         const QString& text);
-
-  void warningDialogClosed();
-
-  void showInfoDialog(const QString& title,
-                      const QString& text);
-
-  // Signals to connect to RVIZ panel/display
-  void showManipulationWidget();
-  void hideManipulationWidget();
-
-  void manipulatorChangedManipulationWidget(QString manipulator);
-  void tcpLinkChangedManipulationWidget(QString tcp_link);
-
-  void showCartesianManipulationWidget();
-  void hideCartesianManipulationWidget();
-
-  void showJointManipulationWidget();
-  void hideJointManipulationWidget();
-
-  void resetToCurrentStateManipulationWidget();
-
-  void showTrajectory();
-  void hideTrajectory();
-
-  // State machine signals
-  void partLocalized();
-  void partLocalizationVerified();
-  void noPartSelected();
-  void partSelected();
-  void noJobSelected();
-  void jobSelected();
-  void planComplete();
-  void jobApproved();
-  void executionComplete();
-  void updateProgressBar(const int);
-
-public Q_SLOTS:
-  void availableManipulatorsChangedManipulationWidget(QStringList manipulators);
-  void availableTCPLinksChangedManipulationWidget(QStringList tcp_links);
+                       std::string database_directory = std::string(std::getenv("HOME")) + "/.local/share/"
+                                                                                           "offline_generated_paths");
 
 protected Q_SLOTS:
 
   void onPartSelected(const std::string);
 
-  void onPartSelectionStatusChanged(const bool status);
-
-  virtual void onPartRemoved(const bool success);
-
-  void onSafelyGoHome();
-
-  void onScanBooth();
-
-  void onAlignToScan();
-
-  void onStartLocalization();
-
-  void onStartLocalizationVerification();
-
-  void onLoadLocLog();
-  
-  void onDetailedScanLoc();
-
-  virtual void onPlanJob();
-
-  virtual void onApproveJob();
-
-  virtual void onExecuteJob();
-
-  void onShowWarningDialog(const QString& title,
-                           const QString& text);
-
-  void onShowInfoDialog(const QString& title,
-                        const QString& text);
-
-  void onCancelCurrentTask();
-
-  void onUpdateProgressBar(const int value);
-
 protected:
-
-  /**
-   * @brief Creates a state machine to manage the properties of the UI (i.e. enabled state, color, etc.).
-   * This state machine is a "nested sequential" type state machine, meaning that states are
-   * connected sequentially by "forward" transitions, and any given state is connected by "reverse"
-   * transitions to all previous states
-   */
-  void createStateMachine();
-
-//  void userControlCallback(const crs_msgs::AllowUserControlGoalConstPtr& goal);
-
-  /* Notification callbacks
-   *
-   * Note: these methods are given as arguments to the application class which eventually calls
-   * them from a ROS thread from within an action callback. Therefore we cannot create a QMessageBox
-   * directly inside them (since this function is not being called by the Qt GUI thread);
-   * thus we must emit a Qt signal to trigger the creation of the dialog window instead.
-   *
-   */
-  void onSafelyWentHome(const bool success);
-  void onScanBoothComplete(const bool success);
-  void onLocalizationFeedback(const int pct_complete);
-  void onLocalizationComplete(const bool success);
-  void onLocalizationVerificationFeedback(const int pct_complete);
-  void onLocalizationVerificationComplete(const bool success);
-  void onJobSelectionStatusChanged(const bool status);
-  void onPlanJobComplete(const bool success);
-  void onApproveJobComplete(const bool approved);
-  void onExecuteJobComplete(const bool success);
-  void onActivePartSet(const bool localized, const bool verified);
-
   Ui::CRSApplication* ui_;
 
-//  QStateMachine* sm_;
-
-  QProgressBar* progress_bar_;
-  QProgressDialog* progress_dialog_;
-
   rclcpp::Node::SharedPtr node_;
-
-//  actionlib::SimpleActionServer<crs_msgs::AllowUserControlAction> user_control_action_server_;
-
-//  crs_application::ApplicationContextBasePtr app_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr current_state_sub_;
+  void currentStateCB(const std_msgs::msg::String::ConstSharedPtr current_state);
 
   PartSelectionWidget* part_selector_widget_;
-//  JobSelectorWidget* job_selector_widget_;
-//  TrajectoryApprovalWidget* trajectory_approval_widget_;
-//  ManualManipulationWidget* manual_manipulation_widget_;
-//  DatabaseLogWidget* database_log_widget_;
+  PolygonAreaSelectionWidget* area_selection_widget_;
 };
 
-} // namespace crs_gui
+}  // namespace crs_gui
 
-#endif // crs_GUI_WIDGETS_APPLICATION_WIDGET_BASE_H
+#endif  // crs_GUI_WIDGETS_APPLICATION_WIDGET_BASE_H
