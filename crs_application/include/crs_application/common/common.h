bb9ab4545b404ca3a0fc66beeb92a405c1de831a
jrgnicho <jrgnichodevel@gmail.com>
2020-02-03 15:52:50 -0600

    added new utility functions for getting and comparing joint state messages

--- crs_application/include/crs_application/common/common.h
+++ crs_application/include/crs_application/common/common.h
@@ -38,6 +38,10 @@
 
 #include <string>
 #include <boost/any.hpp>
+#include <boost/format.hpp>
+#include <rclcpp/node.hpp>
+#include <rclcpp/subscription.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 
 namespace crs_application
 {
@@ -68,6 +72,62 @@ struct ActionResult
   }
 
 };
+
+/**
+ * @brief compares the joints in js1 to js2 and retuns the max difference.  If a joint in js1 is not
+ * found in js2 then -1 will be returned
+ * @param js1 joint state 1
+ * @param js2 joint state 1
+ * @return A double value > 0 or -1 when a joint isn't found.
+ */
+static double compare(const sensor_msgs::msg::JointState& js1, const sensor_msgs::msg::JointState& js2,
+                      std::string& err_msg)
+{
+  double diff = 0.0;
+  for(std::size_t j = 0; j < js1.name.size(); j++)
+  {
+    std::vector<std::string>::const_iterator pos = std::find(js2.name.begin(), js2.name.end(), js1.name[j]);
+    if(pos == js2.name.end())
+    {
+      err_msg = boost::str(boost::format("Joint %s was not found") % js1.name[j]);
+      return -1.0;
+    }
+    std::size_t idx = std::distance(js2.name.begin(), pos);
+    double temp_diff = std::abs(js1.position[j] - js2.position[idx]);
+    diff = temp_diff > diff ? temp_diff : diff;
+  }
+  return diff;
+}
+
+static sensor_msgs::msg::JointState::SharedPtr getCurrentState(std::shared_ptr<rclcpp::Node> node,
+                                                               const std::string topic_name,
+                                                               double timeout)
+{
+  sensor_msgs::msg::JointState::SharedPtr msg = nullptr;
+  std::promise<sensor_msgs::msg::JointState> promise_obj;
+  std::future<sensor_msgs::msg::JointState> fut_obj = promise_obj.get_future();
+
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subs;
+  subs = node->create_subscription<sensor_msgs::msg::JointState>(
+      topic_name,rclcpp::QoS(1),[&promise_obj]
+                                   (const sensor_msgs::msg::JointState::SharedPtr msg) -> void
+  {
+    promise_obj.set_value(*msg);
+  });
+
+  std::future_status sts = fut_obj.wait_for(std::chrono::duration<double>(timeout));
+  subs.reset();
+  /** @warning there's no clean way to close a subscription but according to this issue
+                           https://github.com/ros2/rclcpp/issues/205, destroying the subscription
+                           should accomplish the same */
+  if(sts != std::future_status::ready)
+  {
+    return nullptr;
+  }
+  msg = std::make_shared<sensor_msgs::msg::JointState>(fut_obj.get());
+  return msg;
+}
+
 }
 }
 
