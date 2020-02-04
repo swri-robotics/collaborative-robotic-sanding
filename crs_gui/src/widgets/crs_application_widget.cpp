8f2265fff360d9bd4c6f50cb5b5b147de718bd43
mpowelson <matthew.powelson@swri.org>
2020-02-04 14:25:21 -0600

    Add mesh visualization
    Publishes the mesh as a marker when selected in the gui

--- crs_gui/src/widgets/crs_application_widget.cpp
+++ crs_gui/src/widgets/crs_application_widget.cpp
@@ -32,7 +32,9 @@
 
 const static std::string CURRENT_STATE_TOPIC = "current_state";
 const static std::string GET_AVAILABLE_ACTIONS = "get_available_actions";
+const static std::string GET_CONFIGURATION_SERVICE = "get_configuration";
 const static std::string EXECUTE_ACTION = "execute_action";
+const static std::string MESH_MARKER_TOPIC = "mesh_marker";
 const static double WAIT_FOR_SERVICE_PERIOD = 1.0;
 
 namespace crs_gui
@@ -43,22 +45,73 @@ CRSApplicationWidget::CRSApplicationWidget(rclcpp::Node::SharedPtr node,
   : QWidget(parent)
   , ui_(new Ui::CRSApplication)
   , node_(node)
-  , part_selector_widget_(new PartSelectionWidget())
+  , database_directory_(database_directory)
+  , part_selector_widget_(new PartSelectionWidget(parent, database_directory))
   , area_selection_widget_(new PolygonAreaSelectionWidget(node, "", ""))
   , state_machine_interface_widget_(new StateMachineInterfaceWidget(node))
 {
   ui_->setupUi(this);
+
+  // Set up ROS Interfaces to crs_application
+  auto get_configuration_cb =
+      std::bind(&CRSApplicationWidget::getConfigurationCb, this, std::placeholders::_1, std::placeholders::_2);
+  get_configuration_srv_ =
+      node_->create_service<crs_msgs::srv::GetConfiguration>(GET_CONFIGURATION_SERVICE, get_configuration_cb);
+
+  // Set up ROS interfaces for area selection
+  mesh_marker_pub_.reset();
+  mesh_marker_pub_ = node_->create_publisher<visualization_msgs::msg::MarkerArray>(MESH_MARKER_TOPIC, 1);
+
+  // Visualization Setup
+  {
+    using namespace std::chrono_literals;
+    mesh_marker_timer_ = node_->create_wall_timer(500ms, std::bind(&CRSApplicationWidget::meshMarkerTimerCb, this));
+  }
 
   //  // Add the widgets to the UI
   ui_->vertical_layout_part_selector->addWidget(part_selector_widget_);
   ui_->vertical_layout_area_selection->addWidget(area_selection_widget_);
   ui_->vertical_layout_sm_interface->addWidget(state_machine_interface_widget_);
 
+  // Connect signals and slots
   connect(part_selector_widget_, &PartSelectionWidget::partSelected, this, &CRSApplicationWidget::onPartSelected);
-
-  // TODO: Connect sm widget and remove ui
+  connect(
+      part_selector_widget_, &PartSelectionWidget::partPathSelected, this, &CRSApplicationWidget::onPartPathSelected);
 }
 
-void CRSApplicationWidget::onPartSelected(const std::string selected_part) { std::cout << selected_part << std::endl; }
+void CRSApplicationWidget::onPartSelected(const std::string selected_part)
+{
+  std::cout << "Selected Part: " << selected_part << std::endl;
+  visualization_msgs::msg::Marker marker;
+  marker.header.frame_id = "world";
 
+  std::string path = "file://" + database_directory_ + "/" + selected_part + "/" + selected_part + ".ply";
+  marker.mesh_resource = path;
+  marker.mesh_use_embedded_materials = true;
+  marker.scale.x = 1.0;
+  marker.scale.y = 1.0;
+  marker.scale.z = 1.0;
+  marker.id = 0;
+  marker.type = marker.MESH_RESOURCE;
+  marker.action = marker.MODIFY;
+  visualization_msgs::msg::MarkerArray array;
+  array.markers.push_back(marker);
+  current_mesh_marker_ = array;
+}
+
+void CRSApplicationWidget::onPartPathSelected(const std::string selected_part, const std::string selected_path)
+{
+  // Currently does nothing
+}
+
+void CRSApplicationWidget::getConfigurationCb(crs_msgs::srv::GetConfiguration::Request::SharedPtr req,
+                                              crs_msgs::srv::GetConfiguration::Response::SharedPtr res)
+{
+  // TODO: Write part and send response.
+  res->config.speed = 0.2;
+  //  res->config
+  res->success = true;
+}
+
+void CRSApplicationWidget::meshMarkerTimerCb() { mesh_marker_pub_->publish(current_mesh_marker_); }
 }  // namespace crs_gui
