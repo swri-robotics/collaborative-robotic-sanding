a3141b274cf47283d17b3b0e07a8ce24e5cd4c52
mpowelson <matthew.powelson@swri.org>
2020-02-17 16:04:49 -0600

    Add RVIZ panel for application widget
    This currently crashes when  you click load part for some reason. I'm thinking its some sort of threading issue

---
+++ crs_gui/include/crs_gui/application_panel.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 Southwest Research Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CRS_GUI_APPLICATION_PANEL_H
+#define CRS_GUI_APPLICATION_PANEL_H
+
+#include <rviz_common/panel.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+namespace crs_gui
+{
+class CRSApplicationWidget;
+
+/**
+ * @brief Simple RViz panel that wraps the application widget such that can easily
+ * be used within the context of RViz
+ */
+class ApplicationPanel : public rviz_common::Panel
+{
+  Q_OBJECT
+public:
+  ApplicationPanel(QWidget* parent = nullptr);
+
+  virtual void onInitialize() override;
+
+private:
+  rclcpp::Node::SharedPtr node_;
+
+  std::shared_ptr<CRSApplicationWidget> application_widget_;
+};
+}  // namespace crs_gui
+
+#endif
