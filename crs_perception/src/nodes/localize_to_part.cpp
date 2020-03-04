ee9387030b2247932a3d151b8581a6839688353e
Alex Youngs <ayoungs@swri.org>
2020-03-04 13:32:01 -0600

    Added poses to scans; added debug visualizations

--- crs_perception/src/nodes/localize_to_part.cpp
+++ crs_perception/src/nodes/localize_to_part.cpp
@@ -9,11 +9,16 @@
 #include <tf2_eigen/tf2_eigen.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <crs_msgs/srv/load_part.hpp>
 #include <crs_msgs/srv/localize_to_part.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 #include <rclcpp/rclcpp.hpp>
+
+//delet me
+#include <pcl/io/pcd_io.h>
 
 namespace crs_perception
 {
@@ -26,13 +31,24 @@ public:
     , world_frame_("world")
     , clock_(std::make_shared<rclcpp::Clock>(RCL_ROS_TIME))
     , tf_buffer_(clock_)
+    , tf_listener_(tf_buffer_)
   {
     part_point_cloud_ = boost::make_shared<pcl::PointCloud<pcl::PointXYZ> >();
 
     // parameters
     mesh_num_samples_ = this->declare_parameter("mesh_num_samples", 100000);
-    leaf_size_ = this->declare_parameter("leaf_size", 1.0);
-    part_frame_ = this->declare_parameter("part_frame", "/part");
+    leaf_size_ = this->declare_parameter("leaf_size", 0.01);
+    part_frame_ = this->declare_parameter("part_frame", "part");
+    enable_debug_visualizations_ = this->declare_parameter("enable_debug_visualizations", true);
+
+    // debug visualization publishers
+    if (enable_debug_visualizations_)
+    {
+      // todo(ayoungs: investigate if this behaves like a latched topic
+      loaded_part_pc_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("loaded_part_point_cloud", 1);
+      tf_loaded_part_pc_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("tf_loaded_part_point_cloud", 1);
+      combined_pc_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("combined_scanned_point_clouds", 1);
+    }
 
     // services
     load_part_service_ = this->create_service<crs_msgs::srv::LoadPart>("load_part",
@@ -56,25 +72,31 @@ private:
   double leaf_size_;
   std::string part_frame_;
 
+  // publishers
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr loaded_part_pc_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr tf_loaded_part_pc_pub_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr combined_pc_pub_;
+
   // services
   rclcpp::Service<crs_msgs::srv::LoadPart>::SharedPtr load_part_service_;
   rclcpp::Service<crs_msgs::srv::LocalizeToPart>::SharedPtr localize_to_part_service_;
 
-  std::shared_ptr<rclcpp::Node> node_;
   bool part_loaded_;
   std::string world_frame_;
   rclcpp::Clock::SharedPtr clock_;
   tf2_ros::Buffer tf_buffer_;
-
+  tf2_ros::TransformListener tf_listener_;
 
   pcl::PointCloud<pcl::PointXYZ>::Ptr part_point_cloud_;
+
+  bool enable_debug_visualizations_;
 
   void handleLoadPart(const std::shared_ptr<rmw_request_id_t> request_header,
                       const std::shared_ptr<crs_msgs::srv::LoadPart::Request> request,
                       const std::shared_ptr<crs_msgs::srv::LoadPart::Response> response)
   {
     (void)request_header;
-    RCLCPP_INFO(get_logger(), "Loading part from %s", request->path_to_part.c_str());
+    RCLCPP_INFO(this->get_logger(), "Loading part from %s", request->path_to_part.c_str());
 
     // todo(ayoungs): handle error for bad file
     // todo(ayoungs): make sure part gets loaded correctly after multiple loads
@@ -84,8 +106,17 @@ private:
     part_loaded_ = true;
     response->success = true;
 
-    // todo(ayoungs): should this publish the pointcloud so that other nodes know the original pose or is it assumed to
-    // use the same origin as the CAD file?
+    if (enable_debug_visualizations_)
+    {
+      sensor_msgs::msg::PointCloud2 point_cloud;
+      pcl::toROSMsg(*part_point_cloud_, point_cloud);
+      point_cloud.header.stamp = this->now();
+      point_cloud.header.frame_id = world_frame_;
+      loaded_part_pc_pub_->publish(point_cloud);
+
+      pcl::PCDWriter w;
+      w.writeBinaryCompressed("/home/ayoungs/test_loaded_part.pcd", *part_point_cloud_);
+    }
   }
 
   void handleLocalizeToPart(const std::shared_ptr<rmw_request_id_t> request_header,
@@ -93,24 +124,22 @@ private:
                             const std::shared_ptr<crs_msgs::srv::LocalizeToPart::Response> response)
   {
     (void)request_header;
-    RCLCPP_INFO(node_->get_logger(), "Test 1 ba");
     if (part_loaded_)
     {
       pcl::PointCloud<pcl::PointXYZ>::Ptr combined_point_cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZ> >();
-      RCLCPP_INFO(node_->get_logger(), "Test 1 a");
       for (auto& point_cloud_msg : request->point_clouds)
       {
-      RCLCPP_INFO(node_->get_logger(), "Test 2 a");
         geometry_msgs::msg::TransformStamped transform;
         try
         {
-          transform = tf_buffer_.lookupTransform(world_frame_, point_cloud_msg.header.frame_id, point_cloud_msg.header.stamp);
+          //transform = tf_buffer_.lookupTransform(world_frame_, point_cloud_msg.header.frame_id, point_cloud_msg.header.stamp);
+          transform = tf_buffer_.lookupTransform(world_frame_, point_cloud_msg.header.frame_id, tf2::TimePointZero);
         }
         catch (tf2::TransformException ex)
         {
-          RCLCPP_ERROR(get_logger(), "%s", ex.what());
+          RCLCPP_ERROR(this->get_logger(), "%s", ex.what());
           response->success = false;
-          response->error = "Failed to transform point cloud to " + world_frame_ + " frame";
+          response->error = "Failed to transform point cloud from '" + point_cloud_msg.header.frame_id + "' to '" + world_frame_ + "' frame";
           return;
         }
 
@@ -123,15 +152,24 @@ private:
 
         *combined_point_cloud += *transformed_cloud;
       }
-      RCLCPP_INFO(node_->get_logger(), "Test 3 a");
+
+      if (enable_debug_visualizations_)
+      {
+        sensor_msgs::msg::PointCloud2 point_cloud;
+        pcl::toROSMsg(*combined_point_cloud, point_cloud);
+        point_cloud.header.stamp = this->now();
+        point_cloud.header.frame_id = world_frame_;
+        combined_pc_pub_->publish(point_cloud);
+
+        pcl::PCDWriter w;
+        w.writeBinaryCompressed("/home/ayoungs/test_loaded_part.pcd", *part_point_cloud_);
+      }
 
       pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
       icp.setInputSource(combined_point_cloud);
       icp.setInputTarget(part_point_cloud_);
-      RCLCPP_INFO(node_->get_logger(), "Test 4 a");
       pcl::PointCloud<pcl::PointXYZ> final;
       icp.align(final);
-      RCLCPP_INFO(node_->get_logger(), "Test 5 a");
       // todo(ayoungs): use convergence and fitness score?
       // std::cout << "has converged:" << icp.hasConverged() << " score: " <<
       // icp.getFitnessScore() << std::endl;
@@ -147,6 +185,12 @@ private:
           tf2::Matrix3x3(tm(0, 0), tm(0, 1), tm(0, 2), tm(1, 0), tm(1, 1), tm(1, 2), tm(2, 0), tm(2, 1), tm(2, 2)),
           tf2::Vector3(tm(0, 3), tm(1, 3), tm(2, 3)));
       response->transform.transform = tf2::toMsg(tf2_transform);
+
+      RCLCPP_INFO(this->get_logger(),
+                  "Transform : (%f, %f, %f)",
+                  response->transform.transform.translation.x,
+                  response->transform.transform.translation.y,
+                  response->transform.transform.translation.z);
 
       response->success = true;
     }
