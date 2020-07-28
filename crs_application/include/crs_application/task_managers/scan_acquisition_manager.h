81360d384ef8a951e316d939011a7b1aaf0a67b9
Tyler Marr <tylermarr17@gmail.com>
2020-07-28 11:27:23 -0500

    Updated scan acqusition based on new simulated ctrl change

--- crs_application/include/crs_application/task_managers/scan_acquisition_manager.h
+++ crs_application/include/crs_application/task_managers/scan_acquisition_manager.h
@@ -122,8 +122,6 @@ protected:
   datatypes::ScanAcquisitionResult current_data_;
   uint scan_index_;
 
-  bool force_controlled_trajectories_;
-
   void handlePointCloud(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 };
 
