70d0219e05c410af4ca97c623d8626509518e217
Tyler Marr <tylermarr17@gmail.com>
2020-08-07 10:42:16 -0500

    added simulation of robot io

--- crs_robot_comms/src/ur_comms_sim.cpp
+++ crs_robot_comms/src/ur_comms_sim.cpp
@@ -3,6 +3,7 @@
 #include <std_srvs/srv/set_bool.hpp>
 
 static const std::string CONTROLLER_CHANGER_SERVICE = "compliance_controller_on";
+static const std::string TOGGLE_SANDER_SERVICE = "toggle_sander";
 
 class URRobotCommsSim : public rclcpp::Node
 {
@@ -13,6 +14,9 @@ public:
     controller_changer_service_ = this->create_service<std_srvs::srv::SetBool>(
         CONTROLLER_CHANGER_SERVICE,
         std::bind(&URRobotCommsSim::simControllerChangeCB, this, std::placeholders::_1, std::placeholders::_2));
+    ur_io_service_ = this->create_service<std_srvs::srv::SetBool>(
+        TOGGLE_SANDER_SERVICE,
+        std::bind(&URRobotCommsSim::simUrIoCB, this, std::placeholders::_1, std::placeholders::_2));
   }
 
 private:
@@ -21,7 +25,13 @@ private:
   {
     response->success = true;
   }
+  void simUrIoCB(std::shared_ptr<std_srvs::srv::SetBool::Request> request,
+                 std::shared_ptr<std_srvs::srv::SetBool::Response> response)
+  {
+    response->success = true;
+  }
   rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr controller_changer_service_;
+  rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr ur_io_service_;
 };
 
 int main(int argc, char** argv)
